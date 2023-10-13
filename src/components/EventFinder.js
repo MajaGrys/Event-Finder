5b17468712cb3eceef32aa957c480ad39064193c
MajaGrys <majusiag@poczta.onet.pl>
2023-10-13 17:14:28 +0200

    Added sorting and searching using keywords

--- src/components/EventFinder.js
+++ src/components/EventFinder.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import FoundEvents from './FoundEvents';
 import { RotatingLines } from 'react-loader-spinner';
 
@@ -7,6 +7,7 @@ export default function EventFinder({ city }) {
     const [eventInfo, changeEventInfo] = useState({
       city: city,
       datetime: '',
+      keyword: '',
       sort: ''
     });
     const [searchEvent, setSearchEvent] = useState(eventInfo);
@@ -17,7 +18,7 @@ export default function EventFinder({ city }) {
     async function getData() {
       try {
         setLoading(true);
-        const response = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=200&city=${eventInfo.city}&startDateTime=${eventInfo.datetime}&sort=${eventInfo.sort}&apikey=`); // API key must be added, this is empty for security purposes 
+        const response = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=200&city=${eventInfo.city}&startDateTime=${eventInfo.datetime}&keyword=${eventInfo.keyword}&sort=${eventInfo.sort}&apikey=`); // API key must be added, this is empty for security purposes 
         setData(response.data._embedded.events)
         setError(false);
         setLoading(false);
@@ -39,7 +40,7 @@ export default function EventFinder({ city }) {
     //EventFinder submit handler
     const handleSubmit = (e) => { 
       e.preventDefault();
-      changeEventInfo({city: searchEvent.city, datetime: searchEvent.datetime, sort: searchEvent.sort})
+      changeEventInfo({city: searchEvent.city, datetime: searchEvent.datetime, sort: searchEvent.sort, keyword: searchEvent.keyword})
     }
 
     return (
@@ -51,15 +52,32 @@ export default function EventFinder({ city }) {
       </div>
       
       <form className="events-search d-flex justify-content-center flex-wrap" onSubmit={handleSubmit}>
-        <div className="form-floating mb-3 col-md-2">
+        <div className="form-floating">
+          <input type="text" className="form-control" onChange={e => setSearchEvent(info =>{return {...info, keyword: e.target.value}})}></input>
+          <label htmlFor="city">Enter a keyword</label>
+        </div>
+
+        <div className="form-floating">
           <input type="text" className="form-control" onChange={e => setSearchEvent(info =>{return {...info, city: e.target.value}})}></input>
           <label htmlFor="city">Enter a city</label>
         </div>
 
-        <div className="form-floating mb-3">
+        <div className="form-floating">
           <input type="datetime-local" className="form-control" onChange={e => setSearchEvent(info =>{return {...info, datetime: e.target.value+':00Z'}})}></input>
           <label htmlFor="date">Enter a date</label>
         </div>
+
+        <div className="form-floating mb-3">
+          <select className="form-control" onChange={e => setSearchEvent (info => {return {...info, sort: e.target.value}})}>
+            <option value='relevance,desc'>Relevance</option>
+            <option value='date,asc'>Date (asc)</option>
+            <option value='date,desc'>Date (desc)</option>
+            <option value='name,asc'>Name (asc)</option>
+            <option value='name,desc'>Name (desc)</option>
+          </select>
+          <label htmlFor="date">Sort by</label>
+        </div>
+
         <input type="submit" className="btn btn-primary mb-3" value="Search" />  
       </form>
 
