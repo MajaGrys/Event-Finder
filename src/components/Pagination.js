948a5f873d628bafa45aa9321eb31fa5eb2331bc
MajaGrys <majusiag@poczta.onet.pl>
2023-10-13 10:26:57 +0200

    Added pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,21 @@
+export default function Pagination({currentPage, numberOfPages, setCurrentPage}) {
+    const previousPage = () => {
+        if (currentPage !== 1) {
+          setCurrentPage(currentPage - 1);
+        }
+      }
+  
+    const nextPage = () => {
+      if (currentPage !== numberOfPages) {
+          setCurrentPage(currentPage + 1);
+      }
+    }
+    
+    return (
+        <div className="pagination justify-content-center m-4">
+            {currentPage !== 1 && <a class="page-link change-page" onClick={previousPage}>&laquo;</a>}
+            <a className="page-link current-page">{currentPage} / {numberOfPages}</a>
+            {currentPage !== numberOfPages && <a className="page-link change-page" onClick={nextPage}>&raquo;</a>}
+        </div>
+    )
+}
