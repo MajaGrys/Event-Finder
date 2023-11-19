b12a851f2c8ad942e27d00f391def721c6f7ee7a
MajaGrys <majagrys@wp.pl>
2023-11-19 23:22:12 +0100

    Updated packages and added Cypress

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This project allows users to search for events in the vicinity of the chosen city. [Discovery API](https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/) has been used to provide information about events e.g. date and place.
 
 React and Bootstrap have been used to develop the website. [Append](https://bootstrapmade.com/append-bootstrap-website-template/) template has been adapted into a React app by dividing the page into components and converting plain JavaScript into React Hooks.
+
+The project has been deployed on [GitHub](https://majagrys.github.io/Event-Finder/).
--- src/tests/Nav.test.js
+++ src/tests/Nav.test.js
@@ -1,22 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import Nav from '../components/Nav';
 
-test('Render all links', () => {
+test('Render all links, including the logo', () => {
     render(<Nav />);
     const links = screen.getAllByRole('link');
     expect(links).toHaveLength(6);
 })
 
-test('Render the mobile navigation button', async () => {
-    const onChange = jest.fn();
-    const user = userEvent.setup();
-
+test('Render the mobile navigation button', () => {
     render(<Nav />);
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('button', { class: 'mobile-nav-btn' });
     expect(button).toBeInTheDocument();
-
-    await user.click(button);
-    expect(onChange).toHaveBeenCalledTimes(1);
-    expect(button).toHaveClass('nav-active');
 })
